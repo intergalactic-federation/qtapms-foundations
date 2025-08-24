383b0526d2c0ce7064493f0a9ce7a5699473ecb4
MikeThe00 <mikey.rodino@gmail.com>
2025-08-24 11:52:19 -0500

    Add more to sim

---
+++ build/CMakeFiles/qtapms-sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qtapms-sim.dir/src/lib/glad/src/gl.c.o"
+  "CMakeFiles/qtapms-sim.dir/src/lib/glad/src/gl.c.o.d"
+  "CMakeFiles/qtapms-sim.dir/src/main.cpp.o"
+  "CMakeFiles/qtapms-sim.dir/src/main.cpp.o.d"
+  "qtapms-sim"
+  "qtapms-sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/qtapms-sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- sim/src/main.cpp
+++ sim/src/main.cpp
@@ -2,8 +2,25 @@
 #include <glad/gl.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <cmath>
+
 //libs to make window, use opengl, and out put errors
 using namespace std;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
 
 int main(){
     //init glfw and giving context
@@ -12,6 +29,13 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+    };
+    
     // making window, outputing error, and making contextCurrent window
     GLFWwindow* window = glfwCreateWindow(800, 800, "qtapms-sim", NULL, NULL);
     if (window == NULL)
@@ -29,14 +53,66 @@ int main(){
 
     glViewport(0, 0, 800, 800);
 
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+    0,                          // index
+    3,                          // size
+    GL_FLOAT,                   // type
+    GL_FALSE,                   // normalized
+    (GLsizei)(3 * sizeof(float)), // stride cast to GLsizei
+    (const void*)(uintptr_t)0     // pointer cast through uintptr_t
+    );
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
     //loop for window to stay open and only close if closed
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.7f, 0.13F, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
     
     // destroying Window and terminateing glfw
     glfwDestroyWindow(window);
